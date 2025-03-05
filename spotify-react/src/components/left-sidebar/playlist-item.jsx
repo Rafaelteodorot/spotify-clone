d132d9075e5828f5009b390075a6ea07a191bb93
Rafaelteodorot <rafa.ro785@gmail.com>
2025-03-05 20:29:06 -0300

    queue-item

--- spotify-react/src/components/left-sidebar/playlist-item.jsx
+++ spotify-react/src/components/left-sidebar/playlist-item.jsx
@@ -4,7 +4,7 @@ import { Icon } from '../icons/icon';
 import { IconButton } from '../buttons/icon-button';
 
 
-function PlayListItemRoot(props) {
+function PlaylistItemRoot(props) {
   return (
     <li className="playlist-item flex">
       {props.children}
@@ -12,66 +12,21 @@ function PlayListItemRoot(props) {
   );
 }
 
-PlayListItemRoot.propsType = {
+PlaylistItemRoot.propTypes = {
   children: PropTypes.elementType,
-}
+};
 
-function PlayListItemImage(props) {
+function PlaylistItemImage(props) {
   return (
     <img src={props.src} />
   );
 }
 
-PlayListItemImage.propsType = {
+PlaylistItemImage.propTypes = {
   src: PropTypes.string,
-}
-
-function PlayListItemContent(props) {
-  return <div className="info">{props.children}</div>
-}
-
-PlayListItemContent.propsType = {
-  children: PropTypes.elementType,
-}
-
-function PlayListItemTitle(props) {
-  return <h3 className="text-white">{props.children}</h3>
-}
-
-PlayListItemTitle.propsType = {
-  children: PropTypes.elementType,
-}
-
-function PlayListItemContainerDescription(props) {
-  return <span>{props.children}</span>
-}
-
-PlayListItemContainerDescription.propsType = {
-  children: PropTypes.elementType,
-}
-
-function PlayListItemIcon() {
-  return (
-    <Icon size="xs">
-      <svg className="xs-icon" data-encore-id="icon" role="img" aria-hidden="false" viewBox="0 0 16 16">
-        <title>Fixado</title>
-        <path
-          d="M8.822.797a2.72 2.72 0 0 1 3.847 0l2.534 2.533a2.72 2.72 0 0 1 0 3.848l-3.678 3.678-1.337 4.988-4.486-4.486L1.28 15.78a.75.75 0 0 1-1.06-1.06l4.422-4.422L.156 5.812l4.987-1.337L8.822.797z">
-        </path>
-      </svg>
-    </Icon>
-  );
-}
-
-function PlayListItemDescription(props) {
-  return <span>{props.children}</span>
-}
-
-PlayListItemDescription.propsTypes = {
-  children: PropTypes.children,
 };
 
-function PlayListIconFolder() {
+function PlaylistIconFolder() {
   return (
     <div className="folder">
       <Icon>
@@ -85,7 +40,54 @@ function PlayListIconFolder() {
   );
 }
 
-function PlayListItemButton() {
+function PlaylistItemContent(props) {
+  return <div className="info">{props.children}</div>;
+}
+
+PlaylistItemContent.propTypes = {
+  children: PropTypes.elementType,
+};
+
+function PlaylistItemTitle(props) {
+  return <h3 className="text-white">{props.children}</h3>;
+}
+
+PlaylistItemTitle.propTypes = {
+  children: PropTypes.elementType,
+};
+
+function PlaylistItemContainerDescription(props) {
+  return <span>{props.children}</span>;
+}
+
+PlaylistItemContainerDescription.propTypes = {
+  children: PropTypes.elementType,
+};
+
+function PlaylistItemIcon() {
+  return (
+    <Icon size="xs">
+      <svg className="xs-icon" data-encore-id="icon" role="img" aria-hidden="false" viewBox="0 0 16 16">
+        <title>Fixado</title>
+        <path
+          d="M8.822.797a2.72 2.72 0 0 1 3.847 0l2.534 2.533a2.72 2.72 0 0 1 0 3.848l-3.678 3.678-1.337 4.988-4.486-4.486L1.28 15.78a.75.75 0 0 1-1.06-1.06l4.422-4.422L.156 5.812l4.987-1.337L8.822.797z">
+        </path>
+      </svg>
+    </Icon>
+  );
+}
+
+function PlaylistItemDescription(props) {
+  return (
+  <span>{props.children}</span>
+);
+}
+
+PlaylistItemDescription.propTypes = {
+  children: PropTypes.children,
+};
+
+function PlaylistItemButton() {
   return (
     <IconButton>
       <Icon size="sm">
@@ -94,17 +96,17 @@ function PlayListItemButton() {
         </svg>
       </Icon>
     </IconButton>
-  )
+  );
 }
 
-export const PlayListItem = {
-  Root: PlayListItemRoot,
-  IconFolder: PlayListIconFolder,
-  Image: PlayListItemImage,
-  Content: PlayListItemContent,
-  Title: PlayListItemTitle,
-  ContainerDescription: PlayListItemContainerDescription,
-  Icon: PlayListItemIcon,
-  Description: PlayListItemDescription,
-  Button: PlayListItemButton,
-}
+export const PlaylistItem = {
+  Root: PlaylistItemRoot,
+  IconFolder: PlaylistIconFolder,
+  Image: PlaylistItemImage,
+  Content: PlaylistItemContent,
+  Title: PlaylistItemTitle,
+  ContainerDescription: PlaylistItemContainerDescription,
+  Icon: PlaylistItemIcon,
+  Description: PlaylistItemDescription,
+  Button: PlaylistItemButton,
+};
